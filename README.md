3fd1957de5da2193b38bc02a8455f9cbdb6cd512
mcleddy <maryleddy1@gmail.com>
2019-10-09 23:51:50 -0700

    adjustments to do-this function, and write readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# liri_app
+liri app
+
+The liri app is an application run through node that can look up movies, concerts, and songs through written commands.
+
+The user can search for songs by entering "spotify-this" and the name of a song.
+
+The user can search for concerts by writing "concert-this" and entering the name of an artist.
+
+The user can search for movies by writing "movie-this" and entering the name of a movie.
+
+The user can also bring up text written into the random.txt folder by writing "do-this".
+
+APIs used to find information are Bands in Town, OMDb, and Spotify.
+
+
+**code issues unable to make the call to Bands in Towm work
--- liri.js
+++ liri.js
@@ -30,42 +30,85 @@ function userCommand(userInput, userQuery) {
         case "spotify-this":
             spotifyThis();
             break;
+        case "do-this":
+            doThis();
     }
 }
 userCommand(userInput, userQuery);
 
+//concert search function
+//I dont think that my call is returning anything
 function concertThis() {
-    console.log(`\n---Finding the show for...${userQuery}`);
-    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=441bf070-d681-48da-8be5-16620faac1a0" + bandsIntown.response, body)
-    {
-        let newBand = JSON.parse(body);
-        if (newBand.length > 0) {
-            for (i = 0; i < 1; i++) {
+    console.log(`\nFinding a show for ${userQuery}`);
+    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp" + function (error, response, body) {
+        if (!error && response.statusCode === 200) {
 
-                //console data
-                console.log(`\n Here is your information!: ${newBand[i].lineup[0]}
+            let newBand = JSON.parse(body);
+            if (newBand.length > 0) {
+                for (i = 0; i < 1; i++) {
+
+                    //console data
+                    console.log(`\n Here is your information!: ${newBand[i].lineup[0]}
                 \n Venue: ${newBand[i].venue.name}\n Concert Location: ${newBand[i].venue.city}`)
 
-                let concertDate = moment(newBand[i].datetime).format("MM/DD/YYYY");
-                console.log(`Date and Time: ${concertDate}\n---`);
+                    //marking time
+                    let concertDate = moment(newBand[i].datetime).format("MM/DD/YYYY");
+                    console.log(`Date and Time: ${concertDate}\n---`);
+                };
+            } else {
+                console.log("Sorry, information not found.")
             };
-        } else {
-            console.log("Sorry, information not found.")
         };
-    };
+    })
 }
 
-function spotifyThis(){
-    console.log(`\n--Findig "${userQuery}"`);
+//song search function
+function spotifyThis() {
+    console.log(`\nFindig "${userQuery}"`);
 
-    spotify.search({ type: `track`, query: userQuery, limit: 1}, function (error, data){
+    spotify.search({ type: `track`, query: userQuery, limit: 1 }, function (error, data) {
         if (error) {
             return console.log("error occured");
         }
-        let spotifyAray =data.tracks.items;
-        for (i=0; i< spotifyAray.length; i++){
+        let spotifyAray = data.tracks.items;
+        for (i = 0; i < spotifyAray.length; i++) {
             console.log(`\n Artist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name} \nLink: ${data.tracks.items[i].external_urls.spotify} \nAlbum: ${data.tracks.items[i].album.name}`)
         }
     })
 
 }
+
+//omdb search function
+function movieThis() {
+    console.log(`\nFinding "${userQuery}"`);
+    if (!userQuery) { userQuery = "The Brave Little Toaster" };
+
+    request("http://www.omdbapi.com/?t=" + userQuery + "&y=&plot=short&apikey=982222fc", function (error, response, body) {
+        let newMovie = JSON.parse(body);
+
+        let ratingsArray = newMovie.Ratings
+        if (ratingsArray.length > 2) {
+        }
+        if (!error && response.statusCode === 200) {
+            console.log(`\nTitle: ${newMovie.Title} \nYear Released: ${newMovie.Year} \nRotten Tomatoes Rating: ${newMovie.Ratings[1].Value} \nIMDB Rating: ${newMovie.imdbRating} \nCountry of Production: ${newMovie.Country} \nLanguage: ${newMovie.Language} \nPlot: ${newMovie.Plot} \nActors: ${newMovie.Actors}`)
+        }
+        else {
+            return console.log("Sorry, information not found.")
+        };
+    })
+};
+
+//do what it says function
+function doThis() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        let dataArray = data.split(",");
+
+        userInput = dataArray[0];
+        userQuery = dataArray[1];
+       
+        userCommand(userInput, userQuery);
+    });
+};
